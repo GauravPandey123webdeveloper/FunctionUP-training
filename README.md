8c7aed5aa7146f001373a9eca3f175f56dde6def
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-04-20 17:42:14 +0530

    Nodejs second assignment

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Nodejs Get Api Session
 
 https://docs.google.com/document/d/1bivlpkgtsJfsudBDcXDNHboGjyuAGA6KgzxJKwJSQwI/edit
 
+
 ## Topics covered
 ### Debugger overview
 ### Path Parameters
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,52 +1,37 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common')
-const myUnderscore = require('underscore')
-
-router.get('/test-me', function (req, res) {
-    res.send('This should be working!')
+//problem 1
+router.get('/movies', function (req, res) {
+    res.send(['Doctor strange','Iron man', 'Bhool Bhulyia','kabhi khushi kabhi gum']);
+});
+//problem 2 + problem 3
+router.get('/movies/:indexNumber',function(req,res){
+    let movarr=['Doctor strange','Iron man', 'Bhool Bhulyia','kabhi khushi kabhi gum'];
+    let indx=req.params.indexNumber;
+    if(indx<movarr.length-1&&indx>=0){
+        res.send(movarr[indx]);
+    }
+    else{
+        res.send("Enter the valid index ");
+    } 
+});
+router.get('/films', function (req, res) {
+    let filmsarr=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }];     
+       res.send(filmsarr);
 });
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
-
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
-})
-
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
-})
-
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
-    console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
-})
-
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
-})
-
-
+router.get('')
 module.exports = router;
