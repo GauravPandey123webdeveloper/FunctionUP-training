fc0c7224dfe9bfdde5d5c456b653b4b4c64af409
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-04-25 14:21:07 +0530

    node_assigment_rnd

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,9 +15,63 @@ router.get("/sol2", function (req,res){
     let sumarr=arr.reduce((a,b)=>a+b);
     let missingNumber = sum-sumarr;
     res.send("the missing number is: "+missingNumber);
-
-
 });
+// second assignment
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+router.post("/players", function(req,res){
+    let newplayer={};
+    newplayer.name=req.body.name;
+    newplayer.dob=req.body.dob;
+    newplayer.gender=req.body.gender;
+    newplayer.city=req.body.city;
+    newplayer.sports=req.body.sports;
+    let condition =false;
+    for(let i=0;i<players.length;i++){
+        if(players[i].name==newplayer.name&&players[i].dob==newplayer.dob){
+           condition=true;
+        }
+    }
+    if(condition!=true){
+        players.push(newplayer);
+        return  res.send(players);
+    }else{
+        console.log("player is already exist")
+        return res.send(players)
+        
+    }
+  
+    
+})
 
 
 
