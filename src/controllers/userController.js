2e8637fc44d69fb11f82c1b3897c2756f8ffaa2c
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-04-27 14:52:27 +0530

    books_assign

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,15 @@
-const UserModel= require("../models/userModel")
+const BookModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
+const createBook= async function (req, res) {
     let data= req.body
-    let savedData= await UserModel.create(data)
+    let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+const getBooksData= async function (req, res) {
+    let allBooks= await UserModel.find()
+    res.send({msg: allBooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBook= createBook
+module.exports.getBooksData= getBooksData
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
+router.post("/createBook", UserController.createBook  )
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getBooksData", UserController.getBooksData)
 
 module.exports = router;
