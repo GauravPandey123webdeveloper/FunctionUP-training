3be41d316f475f4dc9647c58811f4e4f2ba7f54e
GauravPandey123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-05-05 09:34:35 +0530

    global_middleware

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
 const app = express();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+app.use( function (req, res, next) {
+     var currentdate = new Date();
+     var datetime = currentdate.getDate() + " "+ (currentdate.getMonth()+1) + " " + currentdate.getFullYear() + " ,"+ currentdate.getHours() + ":"+ currentdate.getMinutes() + ":" + currentdate.getSeconds();
+     let ip= req.ip
+     let url= req.originalUrl
+     console.log(`${datetime},${ip}, ${url}`)
+     next() 
+    }
+    )
 
 app.use('/', route);
-
-
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
